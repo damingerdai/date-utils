30e725ec94eec6942f2b708bbade6f7f31cbe393
明国宾 <mingguobin@live.com>
2019-02-24 21:50:51 +0800

    build: 基于shelljs实现自动化打包部署

--- build.bat
+++
@@ -1,1 +0,0 @@
-npm run dev && npm run build
---
+++ tools/build/build.js
@@ -0,0 +1,16 @@
+const shell = require('shelljs');
+
+shell.rm(`-Rf`, `lib/**`);
+shell.echo('已经清除lib目录');
+if (shell.exec('rollup --config rollup.config.dev.js').code !== 0 ){
+    shell.echo('Error: 开发环境打包失败');
+    shell.exit(1);
+} else {
+    shell.echo('开发环境打包成功');
+}
+if (shell.exec('rollup --config rollup.config.prod.js').code !== 0 ){
+    shell.echo('Error: 生产环境打包失败');
+    shell.exit(1);
+} else {
+    shell.echo('生产环境打包成功');
+}
