5e25c46f37e7070115f216dddfe7ce09a35b6c08
明国宾 <mingguobin@live.com>
2019-12-24 19:22:27 +0800

    build: add build script

---
+++ build.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const shell = require('shelljs');
+const chalk = require('chalk');
+
+const PACKAGE = `date-utils`;
+const NPM_DIR = `dist`;
+const OUT_DIR = `${NPM_DIR}/package`;
+
+shell.echo('start building...');
+
+shell.rm('-Rf', `${NPM_DIR}/*`);
+shell.mkdir('-p', `./${OUT_DIR}`);
+
+shell.cp('-Rf', ['src/*.ts'], `${OUT_DIR}`);
