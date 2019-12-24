1b2bc2d9dc7d06a1e8f393aa0105ab757bd74cd2
明国宾 <mingguobin@live.com>
2019-12-24 20:28:34 +0800

    ci: validate the 'commit-msg' git hooks

--- public_api.ts
+++ public_api.ts
@@ -6,8 +6,4 @@
  * https://github.com/damingerdai/date-utils.git
  */
 
-<<<<<<< HEAD
 export * from './src/date-utils';
-=======
-export * from './src/date-utils';
->>>>>>> refactor: rename index.ts to date-utils.ts
